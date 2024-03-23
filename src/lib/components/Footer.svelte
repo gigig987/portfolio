1d19cde4d38daff494d8854295ebad53c43426dd
Gianluca gini <gigig987@gmail.com>
2024-03-23 13:23:15 +0000

    added copyright

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    let currentYear = new Date().getFullYear()
+</script>
+
+<section class="container-fluid">
+    <p>© {currentYear} — Web app designed and coded by <a href="https://twitter.com/GginiDesign" target="_blank">Gianluca Gini</a> using Svelte + Notion API.</p>
+</section>
+
+<style>
+    section > p  {
+        font-size: small;
+    }
+</style>
