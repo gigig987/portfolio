eae829794ee9ed34301bfcefd882c4d407b92e4f
Gianluca gini <gigig987@gmail.com>
2024-04-09 00:44:35 +0100

    hotfix no redis on home

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -9,22 +9,9 @@ const fetchDataFromNotion = async () => {
 
 export const load: PageServerLoad = async () => {
   try {
-    const data = await redis.get('home-content');
-    if (data) {
-      const parsedData = JSON.parse(data);
-      if (parsedData.blocks && parsedData.blocks.length) {
-        console.log('cached home');
-        return parsedData;
-      }
-    }
 
     console.log('NO cached home, fetching notion');
     const response = await fetchDataFromNotion();
-
-    if (response && response.blocks && !('error' in response)
-    ) {
-      redis.setex('home-content', 3600 * 1, JSON.stringify(response));
-    }
 
     return response;
   } catch (error) {
